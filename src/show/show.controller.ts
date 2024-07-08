5b3f9b179a9a5b3921c04afbc814cd1614e5ad12
xszvvfm <bce0102@gmail.com>
2024-07-08 09:44:55 +0900

    feat: 공연 목록 조회 API, 공연 상세 조회 API
    resolved #5, resolved #6

--- src/show/show.controller.ts
+++ src/show/show.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ShowService } from './show.service';
 import { CreateShowDto } from './dto/create-show.dto';
+import { Category } from './types/showCategory.type';
 
 @Controller('show')
 export class ShowController {
@@ -14,6 +15,28 @@ export class ShowController {
     return {
       message: '공연 등록에 성공했습니다.',
       data: newShow,
+    };
+  }
+
+  /** 공연 목록 조회 **/
+  @Get()
+  async findAll(@Query('category') category: Category) {
+    const findShows = await this.showService.findAll(category);
+
+    return {
+      message: '공연 목록 조회에 성공했습니다.',
+      data: findShows,
+    };
+  }
+
+  /** 공연 상세 조회 **/
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    const findShow = await this.showService.findOne(id);
+
+    return {
+      message: '공연 상세 조회에 성공했습니다.',
+      data: findShow,
     };
   }
 }
--- src/show/show.service.ts
+++ src/show/show.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Show } from './entities/show.entity';
 import { Schedule } from './entities/show-schedule.entity';
 import { Repository } from 'typeorm';
 import { CreateShowDto } from './dto/create-show.dto';
+import { Category } from './types/showCategory.type';
 
 @Injectable()
 export class ShowService {
@@ -26,5 +27,33 @@ export class ShowService {
     });
 
     return newShow;
+  }
+
+  /** 공연 목록 조회 **/
+  async findAll(category: Category) {
+    if (category) {
+      return await this.showRepository.find({
+        where: { category },
+        relations: ['schedules'],
+      });
+    } else {
+      return await this.showRepository.find({
+        relations: ['schedules'],
+      });
+    }
+  }
+
+  /** 공연 상세 조회 **/
+  async findOne(id: number) {
+    const show = await this.showRepository.findOne({
+      where: { id },
+      relations: ['schedules'],
+    });
+
+    if (!show) {
+      throw new NotFoundException('공연을 찾을 수 없습니다.');
+    }
+
+    return show;
   }
 }
